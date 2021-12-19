0c6ae4e608f55a37f05557f08f25b9793ad81f97
AndySave <51698537+AndySave@users.noreply.github.com>
2021-12-19 04:37:08 +0100

    added bishopAttackMasks array and rook attackMasks array

--- src/definitions/slidersAttacks.h
+++ src/definitions/slidersAttacks.h
@@ -2,4 +2,16 @@
 #ifndef CHESSENGINE2_SLIDERSATTACKS_H
 #define CHESSENGINE2_SLIDERSATTACKS_H
 
-#endif //CHESSENGINE2_SLIDERSATTACKS_H
+#include "../definitions/std_typedef.h"
+#include "../utils/bitops.h"
+
+ull incrementalBishopAttack(int sq);
+ull incrementalRookAttack(int sq);
+
+void initBishopAttackMasks();
+void initRookAttackMasks();
+
+inline ull bishopAttackMasks[64];
+inline ull rookAttackMasks[64];
+
+#endif
