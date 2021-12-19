a35067efa49cfa65a834fd711804a4d3ecc9a65a
AndySave <51698537+AndySave@users.noreply.github.com>
2021-12-19 08:07:32 +0100

    added functions; getBit and getLSB (LeastSignificantBit)

--- src/utils/bitops.h
+++ src/utils/bitops.h
@@ -10,6 +10,10 @@ inline ull clearMask[64];
 void initBitMasks();
 void printBitboard(ull bb);
 
+inline int getBit(ull &bitBoard, int sq){
+    return bitBoard & setMask[sq] ? 1 : 0;
+}
+
 inline void clearBit(ull &bitBoard, int sq){
     bitBoard &= clearMask[sq];
 }
@@ -26,6 +30,13 @@ inline int countBits(ull bitBoard){
         bitBoard &= bitBoard - 1;
     }
     return count;
+}
+
+// Gets index of Least Significant Bit
+inline int getLSB(ull bb){
+    if (bb != 0)
+        return countBits((bb & -bb) - 1);
+    return -1;
 }
 
 #endif
