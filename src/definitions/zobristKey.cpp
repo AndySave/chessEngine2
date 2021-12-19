1caf2ff320931b01a2bca4347e6fb9fb4ffa5e57
AndySave <51698537+AndySave@users.noreply.github.com>
2021-12-19 21:59:27 +0100

    Added file

--- src/definitions/zobristKey.cpp
+++ src/definitions/zobristKey.cpp
@@ -1,2 +1,57 @@
 
 #include "zobristKey.h"
+
+
+ull rand64(){
+    ull n = (ull)rand() + ((ull)rand() << 15) + ((ull)rand() << 30) + ((ull)rand() << 45) + (((ull)rand() & 0xf) << 60);
+    return n;
+}
+
+ull pieceKeys[13][64];
+ull sideKey;
+ull castleKeys[16];
+
+
+void initHashkeys(){
+    for (int pce = 0; pce < 13; pce++){
+        for (int sq = 0; sq < 64; sq++){
+            pieceKeys[pce][sq] = rand64();
+        }
+    }
+
+    sideKey = rand64();
+
+    for (int i = 0; i < 16; i++){
+        castleKeys[i] = rand64();
+    }
+}
+
+
+ull generateHash(Board *brd){
+    ull key = 0;
+
+    for (int pce = 0; pce < 12; pce++){
+        ull bb = brd->bitboards[pce];
+
+        while (bb){
+            ull index = getLSB(bb);
+            clearBit(bb, index);
+            key ^= pieceKeys[pce][index];
+        }
+    }
+
+
+    if (brd->side == white){
+        key ^= sideKey;
+    }
+
+
+    if (brd->enpassantSq != noSq){
+        key ^= pieceKeys[13][sq64(brd->enPas)];
+    }
+
+    key ^= castleKeys[brd->castle];
+
+    return key;
+}
+
--- src/definitions/zobristKey.h
+++ src/definitions/zobristKey.h
@@ -2,4 +2,13 @@
 #ifndef CHESSENGINE2_ZOBRISTKEY_H
 #define CHESSENGINE2_ZOBRISTKEY_H
 
+#include "board.h"
+#include "../utils/bitops.h"
+#include "std_typedef.h"
+#include <random>
+
+ull rand64();
+
+void initHashkeys();
+
 #endif
