c880751a35777b9f9c3e496401f1639376c6d71a
AndySave <51698537+AndySave@users.noreply.github.com>
2021-12-19 22:20:39 +0100

    Added enpassantKeys

--- src/definitions/zobristKey.cpp
+++ src/definitions/zobristKey.cpp
@@ -7,13 +7,14 @@ ull rand64(){
     return n;
 }
 
-ull pieceKeys[13][64];
+ull pieceKeys[12][64];
 ull sideKey;
 ull castleKeys[16];
+ull enpassantKeys[64];
 
 
 void initHashkeys(){
-    for (int pce = 0; pce < 13; pce++){
+    for (int pce = 0; pce < 12; pce++){
         for (int sq = 0; sq < 64; sq++){
             pieceKeys[pce][sq] = rand64();
         }
@@ -23,6 +24,10 @@ void initHashkeys(){
 
     for (int i = 0; i < 16; i++){
         castleKeys[i] = rand64();
+    }
+
+    for (int i = 0; i < 64; i++){
+        enpassantKeys[i] = rand64();
     }
 }
 
@@ -47,7 +52,7 @@ ull generateHash(Board *brd){
 
 
     if (brd->enpassantSq != noSq){
-        key ^= pieceKeys[13][sq64(brd->enPas)];
+        key ^= enpassantKeys[sq64(brd->enPas)];
     }
 
     key ^= castleKeys[brd->castle];
