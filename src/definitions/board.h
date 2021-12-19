415e9c89f8d8248a1d284391900179f8b1bcce8a
AndySave <51698537+AndySave@users.noreply.github.com>
2021-12-19 21:59:16 +0100

    Created Board struct

--- src/definitions/board.h
+++ src/definitions/board.h
@@ -2,6 +2,16 @@
 #ifndef CHESSENGINE2_BOARD_H
 #define CHESSENGINE2_BOARD_H
 
+#include "std_typedef.h"
 
+struct Board{
+    ull bitboards[12];
+    ull occupancies[3];
+
+    int side;
+    int enpassantSq = noSq;
+    int castle;
+
+};
 
 #endif
