26f29d2b05e56f36f13578eef3d30a69ab23c407
AndySave <51698537+AndySave@users.noreply.github.com>
2021-12-20 20:05:24 +0100

    Added part of test

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -1,9 +1,70 @@
+#include "../src/definitions/slidersAttacks.h"
 #include <gtest/gtest.h>
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-// Expect two strings not to be equal.
-EXPECT_STRNE("hello", "world");
-// Expect equality.
-EXPECT_EQ(7 * 6, 42);
+/*
+cout << "Magic number#" << sq << ": " << magic << " failed!" << endl;
+cout << "Index " << indices[occ]-1 << " collided with index " << i << endl;
+*/
+
+/*
+bool magicOk(ull bb, ull magic, int sq){
+    sq++;
+    int bitsInAttackMask = countBits(bb);
+
+    int indices[1 << bitsInAttackMask];
+    for (int i = 0; i < (1 << bitsInAttackMask); i++){
+        indices[i] = 0;
+    }
+
+    for (int i = 0; i < (1 << bitsInAttackMask); i++){
+        ull occ = generateOccupancy(bb, i);
+        occ *= magic;
+        occ >>= (64-bitsInAttackMask);
+
+        if (indices[occ]){
+            return false;
+        }
+
+        indices[occ] = sq;
+    }
+
+    return true;
 }
+
+
+TEST(bishopMagicsTest, TEST) {
+
+    bool ans = true;
+
+    for (int sq = 0; sq < 64; sq++){
+        ull magic = bishopMagics[sq];
+        ull bb = bishopAttackMasks[sq];
+
+        ans &= magicOk(bb, magic, sq);
+    }
+
+    EXPECT_TRUE(ans);
+}
+
+
+TEST(rookMagicsTest, TEST) {
+
+    bool ans = true;
+
+    for (int sq = 0; sq < 64; sq++){
+        ull magic = rookMagics[sq];
+        ull bb = rookAttackMasks[sq];
+
+        ans &= magicOk(bb, magic, sq);
+    }
+
+    EXPECT_TRUE(ans);
+}
+
+*/
+
+
+
+
+
+
