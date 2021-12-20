ccd5a74c85b6d7c9c786ada748dcb53b5b949cf5
AndySave <51698537+AndySave@users.noreply.github.com>
2021-12-20 18:24:45 +0100

    Added includes

--- src/definitions/moveGeneration.h
+++ src/definitions/moveGeneration.h
@@ -2,4 +2,14 @@
 #ifndef CHESSENGINE2_MOVEGENERATION_H
 #define CHESSENGINE2_MOVEGENERATION_H
 
-#endif //CHESSENGINE2_MOVEGENERATION_H
+#include "nonSlidersAttacks.h"
+#include "board.h"
+#include "slidersAttacks.h"
+#include "std_typedef.h"
+#include <iostream>
+
+bool isSquareAttacked(Board *brd, int sq, bool whiteAttacking);
+void printAttackedSquares(Board *brd, bool whiteAttacking);
+
+
+#endif
