e56cab9c62938d97f4cf27ca465247bde7590e42
AndySave <51698537+AndySave@users.noreply.github.com>
2021-12-20 12:18:26 +0100

    Added macros hashPiece, hashSide, hashCastle and hashEnpassant

--- src/definitions/zobristKey.h
+++ src/definitions/zobristKey.h
@@ -7,8 +7,20 @@
 #include "std_typedef.h"
 #include <random>
 
+#define hashPiece(key, pce, sq) key ^= pieceKeys[pce][sq];
+#define hashSide(key) key ^= sideKey;
+#define hashCastle(key, castle) key ^= castleKeys[castle]
+#define hashEnpassant(key, enpassantSq) key ^= enpassantKeys[enpassantSq];
+
+extern ull pieceKeys[12][64];
+extern ull sideKey;
+extern ull castleKeys[16];
+extern ull enpassantKeys[64];
+
 ull rand64();
 
 void initHashkeys();
+
+ull generateHash(Board *brd);
 
 #endif
