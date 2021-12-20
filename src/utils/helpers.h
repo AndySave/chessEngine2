31f058661e00fb6978d3990718a55f8c3e7a21ba
AndySave <51698537+AndySave@users.noreply.github.com>
2021-12-20 12:20:41 +0100

    Created file

---
+++ src/utils/helpers.h
@@ -0,0 +1,13 @@
+
+#ifndef CHESSENGINE2_HELPERS_H
+#define CHESSENGINE2_HELPERS_H
+
+#include <string>
+#include "../definitions/std_typedef.h"
+
+int charToPiece(char c);
+char pieceToChar(int piece);
+int algebraicToSq(string &coords);
+
+
+#endif
