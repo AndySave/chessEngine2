60104a55024dddc9cab035514ecdcd1064fd72d9
AndySave <51698537+AndySave@users.noreply.github.com>
2021-12-20 12:16:46 +0100

    Added include guard and printBoard function

---
+++ src/utils/board_utils.h
@@ -0,0 +1,12 @@
+#ifndef CHESSENGINE2_UTILS_H
+#define CHESSENGINE2_UTILS_H
+
+#include "../definitions/board.h"
+#include "helpers.h"
+#include "bitops.h"
+#include <iostream>
+#include <bitset>
+
+void printBoard(Board *brd);
+
+#endif
