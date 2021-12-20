b16a080e6081df1487ab14e861779f06caee74d5
AndySave <51698537+AndySave@users.noreply.github.com>
2021-12-20 18:25:50 +0100

    Moved tests from main

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -6,81 +6,19 @@
 #include "../utils/fen.h"
 #include "../definitions/board.h"
 #include "../utils/board_utils.h"
-
-bool magicOk(ull bb, ull magic, int sq, bool debugMsg){
-    sq++;
-    int bitsInAttackMask = countBits(bb);
-
-    int indices[1 << bitsInAttackMask];
-    for (int i = 0; i < (1 << bitsInAttackMask); i++){
-        indices[i] = 0;
-    }
-
-    for (int i = 0; i < (1 << bitsInAttackMask); i++){
-        ull occ = generateOccupancy(bb, i);
-        occ *= magic;
-        occ >>= (64-bitsInAttackMask);
-
-        if (indices[occ]){
-            if (debugMsg){
-                cout << "Magic number#" << sq << ": " << magic << " failed!" << endl;
-                cout << "Index " << indices[occ]-1 << " collided with index " << i << endl;
-            }
-            return false;
-        }
-
-        indices[occ] = sq;
-    }
-
-    return true;
-}
-
-
-// TODO: Make unit test
-bool bishopMagicsTest(){
-    cout << "Starting bishop magics test" << endl;
-    for (int sq = 0; sq < 64; sq++){
-        ull magic = bishopMagics[sq];
-        ull bb = bishopAttackMasks[sq];
-
-        if (!magicOk(bb, magic, sq, true)){
-            return false;
-        }
-    }
-
-    cout << "All magic numbers work" << endl;
-    return true;
-}
-
-// TODO: Make unit test
-bool rookMagicsTest(){
-    cout << "Starting rook magics test" << endl;
-    for (int sq = 0; sq < 64; sq++){
-        ull magic = rookMagics[sq];
-        ull bb = rookAttackMasks[sq];
-
-        if (!magicOk(bb, magic, sq, true)){
-            return false;
-        }
-    }
-
-    cout << "All magic numbers work" << endl;
-    return true;
-}
+#include "../definitions/moveGeneration.h"
 
 
 int main() {
 
     initBitMasks();
-    initPawnAttacks();
+    initWhitePawnAttacks();
+    initBlackPawnAttacks();
     initKnightAttacks();
     initKingAttacks();
     initBishopAttackMasks();
     initRookAttackMasks();
     initHashkeys();
-
-    bishopMagicsTest();
-    rookMagicsTest();
 
     fillBishopAttacksArray();
     fillRookAttacksArray();
@@ -90,9 +28,9 @@ int main() {
     string fenString = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     FEN(&board, fenString);
     printBoard(&board);
+
+    printAttackedSquares(&board, true);
 }
-
-
 
 
 
