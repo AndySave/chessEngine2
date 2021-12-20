9c55b8432b54aac514a4a375ec6ac77b142d65d6
AndySave <51698537+AndySave@users.noreply.github.com>
2021-12-20 12:17:17 +0100

    Added FEN

--- src/utils/fen.h
+++ src/utils/fen.h
@@ -2,4 +2,12 @@
 #ifndef CHESSENGINE2_FEN_H
 #define CHESSENGINE2_FEN_H
 
-#endif //CHESSENGINE2_FEN_H
+#include "../definitions/board.h"
+#include "../utils/bitops.h"
+#include "../definitions/zobristKey.h"
+#include "helpers.h"
+#include <string>
+
+void FEN(Board *brd, const string &fenStr);
+
+#endif
