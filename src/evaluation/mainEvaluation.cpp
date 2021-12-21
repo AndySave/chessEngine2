771d5307e77a8b7c33e323d511b88f4f72c61ab8
AndySave <51698537+AndySave@users.noreply.github.com>
2021-12-21 22:37:45 +0100

    Added main eval and temporary material evaluation

--- src/evaluation/mainEvaluation.cpp
+++ src/evaluation/mainEvaluation.cpp
@@ -1,2 +1,31 @@
 
 #include "mainEvaluation.h"
+
+
+int pceScore[12] = {1, 3, 3, 5, 9, 100, 1, 3, 3, 5, 9, 100};
+int material(Board *brd){
+    int score = 0;
+
+    for (int pce = P; pce <= K; pce++){
+        score += countBits(brd->bitboards[pce]) * pceScore[pce];
+    }
+
+    for (int pce = p; pce <= k; pce++){
+        score -= countBits(brd->bitboards[pce]) * pceScore[pce];
+    }
+
+    return score;
+}
+
+
+int eval(Board *brd){
+    int score = 0;
+
+    score += material(brd);
+
+    if (brd->side == white){
+        return score;
+    }
+    return -score;
+}
+
