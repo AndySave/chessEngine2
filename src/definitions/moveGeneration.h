ab88766b101c69644cfaf9a89e98e1d6cd968459
AndySave <51698537+AndySave@users.noreply.github.com>
2021-12-21 17:13:12 +0100

    Removed redundant definitions

--- src/definitions/moveGeneration.h
+++ src/definitions/moveGeneration.h
@@ -10,10 +10,8 @@
 #include "moveStructs.h"
 #include "move.h"
 
-bool isSquareAttacked(Board *brd, int sq, bool whiteAttacking);
-void printAttackedSquares(Board *brd, bool whiteAttacking);
 
-void generateMoves(Board *brd);
+void generateMoves(Board *brd, Movelist *lst);
 
 
 #endif
