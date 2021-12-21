7fc4ec856ca4e4214d292debe5a22aa39e011064
AndySave <51698537+AndySave@users.noreply.github.com>
2021-12-21 22:37:56 +0100

    Added includes

--- src/evaluation/mainEvaluation.h
+++ src/evaluation/mainEvaluation.h
@@ -2,4 +2,9 @@
 #ifndef CHESSENGINE2_MAINEVALUATION_H
 #define CHESSENGINE2_MAINEVALUATION_H
 
+#include "../definitions/board.h"
+#include "../definitions/std_typedef.h"
+
+int eval(Board *brd);
+
 #endif
