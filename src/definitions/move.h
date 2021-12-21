522f788529038e5a6476fc27763f7792758afb92
AndySave <51698537+AndySave@users.noreply.github.com>
2021-12-21 11:45:54 +0100

    Added castlePerm array

--- src/definitions/move.h
+++ src/definitions/move.h
@@ -6,12 +6,26 @@
 #include "../utils/helpers.h"
 #include "board.h"
 #include "zobristKey.h"
+#include "moveGeneration.h"
 
 //Macros to extract information from moveVal (value of a move)
 #define fromSquare(move) (move & 0x3f)
 #define toSquare(move) ((move >> 6) & 0x3f)
 #define promotedPiece(move) ((move >> 12) & 0x3)
 #define moveFlag(move) ((move >> 14) & 0x3)
+
+// Used to update castle permissions
+const int castlePerm[64] ={
+        13, 15, 15, 15, 12, 15, 15, 14,
+        15, 15, 15, 15, 15, 15, 15, 15,
+        15, 15, 15, 15, 15, 15, 15, 15,
+        15, 15, 15, 15, 15, 15, 15, 15,
+        15, 15, 15, 15, 15, 15, 15, 15,
+        15, 15, 15, 15, 15, 15, 15, 15,
+        15, 15, 15, 15, 15, 15, 15, 15,
+        7, 15, 15, 15, 3, 15, 15, 11
+};
+
 
 /*
  * A move consists of 16 bits:
@@ -47,5 +61,9 @@ void printMove(Move& move);
  * Creates a move value (unsigned short) from input fromSq, toSq, promotedPiece and moveFlag.
  */
 unsigned short createMove(int fromSq, int toSq, int promotedPiece, int moveFlag);
+void addPiece(Board *brd, int sq, int piece);
+void clearPiece(Board *brd, int sq);
+bool makeMove(Board *brd, unsigned short move);
+void undoMove(Board *brd);
 
-#endif //CHESSENGINE2_MOVE_H
+#endif
