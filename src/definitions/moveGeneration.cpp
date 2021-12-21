0152e890156bed00408038c642fd1a33af06f8ea
AndySave <51698537+AndySave@users.noreply.github.com>
2021-12-21 17:12:57 +0100

    Moved functions isSquareAttacked and printAttackedSquares

--- src/definitions/moveGeneration.cpp
+++ src/definitions/moveGeneration.cpp
@@ -1,69 +1,6 @@
 
 #include "moveGeneration.h"
 
-bool isSquareAttacked(Board *brd, int sq, bool whiteAttacking){
-    ull occ = brd->occupancies[both];
-    if (whiteAttacking){
-        if (bishopAttackRay(occ, sq) & (brd->bitboards[B] | brd->bitboards[Q])){
-            return true;
-        }
-        if (rookAttackRay(occ, sq) & (brd->bitboards[R] | brd->bitboards[Q])){
-            return true;
-        }
-        if (knightAttacks[sq] & brd->bitboards[N]){
-            return true;
-        }
-        if (kingAttacks[sq] & brd->bitboards[K]){
-            return true;
-        }
-        if (blackPawnAttacks[sq] & brd->bitboards[P]){
-            return true;
-        }
-
-    }else{
-        if (bishopAttackRay(occ, sq) & (brd->bitboards[b] | brd->bitboards[q])){
-            return true;
-        }
-        if (rookAttackRay(occ, sq) & (brd->bitboards[r] | brd->bitboards[q])){
-            return true;
-        }
-        if (knightAttacks[sq] & brd->bitboards[n]){
-            return true;
-        }
-        if (kingAttacks[sq] & brd->bitboards[k]){
-            return true;
-        }
-        if (whitePawnAttacks[sq] & brd->bitboards[p]){
-            return true;
-        }
-    }
-
-    return false;
-}
-
-// Prints representation of the board where attacked squares are 'X' and if not '.'
-void printAttackedSquares(Board *brd, bool whiteAttacking){
-    for (int rank = 7; rank >= 0; rank--){
-        cout << rank+1 << "  ";
-        for (int file = 0; file < 8; file++){
-            int sq = rank*8 + file;
-            if (isSquareAttacked(brd, sq, whiteAttacking)){
-                cout << "X ";
-            }else{
-                cout << ". ";
-            }
-
-        }
-        cout << endl;
-    }
-
-    cout << endl << "   ";
-    for (int i = 0; i < 8; i++){
-        cout << char(97+i) << " ";
-    }
-    cout << endl;
-}
-
 
 void generateMoves(Board *brd, Movelist *lst){
     lst->count = 0;
