edeee2b9fe52b9dcbd2392ba4c76a0eef81677d8
AndySave <51698537+AndySave@users.noreply.github.com>
2021-12-22 17:52:59 +0100

    Fixed memory issue with perft function

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -8,6 +8,46 @@
 #include "../definitions/moveGeneration.h"
 #include "../evaluation/mainEvaluation.h"
 
+
+long long count = 0;
+long long totCount = 0;
+
+void perftRec(Board* brd, int depth) {
+    if (depth <= 0) { // Base case
+        count++;
+        totCount++;
+        return;
+    }
+
+    Movelist moveList;
+    generateMoves(brd, &moveList);
+
+    for (int i = 0 ; i<moveList.count; i++) {
+        bool legalMove = makeMove(brd, moveList.moves[i].move);
+        if (legalMove) {
+            perftRec(brd, depth-1);
+            undoMove(brd);
+        }
+    }
+}
+
+void perft(Board* brd, int depth) {
+    Movelist l;
+    generateMoves(brd, &l);
+
+    for (int i = 0; i<l.count; ++i) {
+        makeMove(brd, l.moves[i].move);
+        perftRec(brd, depth-1);
+        undoMove(brd);
+        cout << i+1 << ", " << count << "\n";
+        printMove(l.moves[i]);
+        cout << "\n\n";
+        count =  0;
+    }
+
+    cout << totCount << endl;
+}
+
 int main() {
     initBitMasks();
     initWhitePawnAttacks();
@@ -26,4 +66,15 @@ int main() {
     string fenString = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     FEN(&board, fenString);
     printBoard(&board);
+
+    //makeMove(&board, createMove(b2, b3, 0, noFlag));
+    //makeMove(&board, createMove(e7, e5, 0, noFlag));
+
+
+    // Late inits
+    initPhase(&board);
+
+    for (int i = 0; i < 1; i++){
+        perft(&board, 6);
+    }
 }
