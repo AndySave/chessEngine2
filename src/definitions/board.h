8ad68feb442466cce6e5f3c7e02cdd68d903d395
ask-r0 <75451424+ask-r0@users.noreply.github.com>
2021-12-22 22:34:10 +0100

    Added comments to board.h

--- src/definitions/board.h
+++ src/definitions/board.h
@@ -9,31 +9,31 @@
 #include <bitset>
 #include "moveStructs.h"
 
-
+// Board structure.
 struct Board{
-    ull bitboards[12];
-    ull occupancies[3];
+    ull bitboards[12]; // Bitboards hold the positioning of pieces. One index for each piece.
+    ull occupancies[3]; // Squares occupied by: 0 - white, 1 - black, 2 - both black and white.
 
-    int side;
-    int enpassantSq = noSq;
-    int castle;
+    int side; // 0 = white, 1 = black
+    int enpassantSq = noSq; //En passant square
+    int castle; // Holds the current possibility of castling for black and white
 
-    // Halfmoves since game started
-    int ply;
-    // Fullmoves since game started
-    int move;
-    // Halfmoves since last capture or pawn move
-    int fiftyMove;
+    int ply; // Halfmoves since game started
+    int move; // Fullmoves since game started
+    int fiftyMove;  // Halfmoves since last capture or pawn move
 
-    int whiteKingPos;
-    int blackKingPos;
+    int whiteKingPos; // Position of white king
+    int blackKingPos; // Position of black king
 
-    ull hashKey = 0ull;
+    ull hashKey = 0ull; // Key of current board state
 
-    Undo history[2048];
+    Undo history[2048]; // History of previous moves
 };
 
+// Prints board in a nice format
 void printBoard(Board *brd);
+
+// Resets a board to empty
 void cleanBoard(Board *brd);
 
 #endif
