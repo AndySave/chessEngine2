f3c799488fe01d79f21387ffe33416d4ee3a17c9
AndySave <51698537+AndySave@users.noreply.github.com>
2021-12-22 20:49:34 +0100

    added values materialMG, materialEG and arrays pieceValuesMG and EG

--- src/evaluation/mainEvaluation.h
+++ src/evaluation/mainEvaluation.h
@@ -8,23 +8,27 @@
 
 
 /// --------------------TAPERED VALUES-------------------- ///
-constexpr inline int tapered[12] = {0, 1, 1, 2, 4, 0, 0, 1, 1, 2, 4, 0};
+inline int phase = 0;
 // totalPhase = tapered[P]*16 + tapered[K]*4 + tapered[B]*4 + tapered[R]*4 + tapered[Q]*2;
 inline int totalPhase = 24;
-inline int phase = 0;
+inline int taperedFactor = 256;
+constexpr inline int tapered[12] = {0, 1, 1, 2, 4, 0, 0, 1, 1, 2, 4, 0};
 
 /// --------------------PIECE VALUES-------------------- ///
-enum values {
-    pawnValueMg   = 126,   pawnValueEg   = 208,
-    knightValueMg = 781,   knightValueEg = 854,
-    bishopValueMg = 825,   bishopValueEg = 915,
-    rookValueMg   = 1276,  rookValueEg   = 1380,
-    queenValueMg  = 2538,  queenValueEg  = 2682
-};
+inline int materialMG = 0;
+inline int materialEG = 0;
+constexpr inline int pieceValuesMg[12] = {126, 781, 825, 1276, 2538, 0, 126, 781, 825, 1276, 2538, 0};
+constexpr inline int pieceValuesEg[12] = {208, 854, 915, 1380, 2681, 0, 208, 854, 915, 1380, 2681, 0};
 
-
-
+/// --------------------INIT FUNCTIONS-------------------- ///
 void initPhase(Board *brd);
+void initMaterial(Board *brd);
+
+/// --------------------UPDATE VALUE FUNCTIONS-------------------- ///
+void addMaterial(int piece);
+void removeMaterial(int piece);
+
+/// --------------------MAIN FUNCTIONS-------------------- ///
 int eval(Board *brd);
 
 
