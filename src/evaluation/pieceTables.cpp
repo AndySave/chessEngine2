a5e890fc8f8808a8524ae601b38dcce58a85f79c
AndySave <51698537+AndySave@users.noreply.github.com>
2021-12-22 22:18:49 +0100

    Added initPieceTable. addPieceTableScore and removePieceTableScore

---
+++ src/evaluation/pieceTables.cpp
@@ -0,0 +1,38 @@
+#include "pieceTables.h"
+
+void initPieceTable(Board *brd){
+    for (int pce = P; pce <= k; pce++){
+        ull piecePositions = brd->bitboards[pce];
+
+        while (piecePositions){
+            int piecePos = getLSB(piecePositions);
+            clearBit(piecePositions, piecePos);
+
+            addPieceTableScore(pce, piecePos);
+        }
+    }
+}
+
+void addPieceTableScore(int piece, int sq){
+    int color = piece <= K ? white : black;
+    if (color == white){
+        pieceTableScoreMG += pieceTables[piece][sq][0];
+        pieceTableScoreEG += pieceTables[piece][sq][1];
+
+    }else{
+        pieceTableScoreMG += pieceTables[piece][getMirror(sq)][0];
+        pieceTableScoreEG += pieceTables[piece][getMirror(sq)][1];
+    }
+}
+
+void removePieceTableScore(int piece, int sq){
+    int color = piece <= K ? white : black;
+    if (color == white){
+        pieceTableScoreMG -= pieceTables[piece][sq][0];
+        pieceTableScoreEG -= pieceTables[piece][sq][1];
+
+    }else{
+        pieceTableScoreMG -= pieceTables[piece][getMirror(sq)][0];
+        pieceTableScoreEG -= pieceTables[piece][getMirror(sq)][1];
+    }
+}
