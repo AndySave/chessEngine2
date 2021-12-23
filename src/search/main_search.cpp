b885452b69be8d1d3f73242deeae9f47c4d61060
AndySave <51698537+AndySave@users.noreply.github.com>
2021-12-23 01:50:02 +0100

    Removed miniMax

--- src/search/main_search.cpp
+++ src/search/main_search.cpp
@@ -1,67 +1,7 @@
 #include "main_search.h"
 
-int miniMax(Board *brd, int depth) {
-    int side = brd->side;
-    Movelist l;
-    generateMoves(brd, &l);
 
-    if (brd->fiftyMove == 100) return 0;
-    if (depth == 0) return eval(brd);
-
-    if (side == white) {
-        int maxEval = -INF;
-
-        bool legalMoveExists = false;
-        for (int i = 0; i<l.count; i++) {
-            bool legalMove = makeMove(brd, l.moves[i].move);
-            if (!legalMove) continue;
-
-            legalMoveExists = true;
-
-            int eval =  miniMax(brd, depth-1);
-            undoMove(brd);
-
-            maxEval = max(maxEval, eval);
-        }
-
-        if (!legalMoveExists) {
-            if (isSquareAttacked(brd, brd->whiteKingPos, false)) {
-                return -mateScore-depth;
-            } else {
-                return 0;
-            }
-        }
-
-        return maxEval;
-
-    } else {
-        int minEval = INF;
-
-        bool legalMoveExists = false;
-        for (int i = 0; i<l.count; i++) {
-            bool legalMove = makeMove(brd, l.moves[i].move);
-            if (!legalMove) continue;
-
-            legalMoveExists = true;
-            int eval = miniMax(brd, depth-1);
-            undoMove(brd);
-
-            minEval = min(minEval, eval);
-        }
-
-        if (!legalMoveExists) {
-            if (isSquareAttacked(brd, brd->blackKingPos, true)) {
-                return mateScore+depth;
-            } else {
-                return 0;
-            }
-        }
-
-        return minEval;
-    }
-}
 int searchCounter = 0;
-
 int askMax(Board *brd, int depth, int alpha, int beta) {
     if (brd->fiftyMove == 100) return 0;
 
