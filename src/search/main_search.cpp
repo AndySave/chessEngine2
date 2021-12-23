408818728c5a077f31f74e2655763a630290f35b
AndySave <51698537+AndySave@users.noreply.github.com>
2021-12-23 22:24:22 +0100

    Fixed quiescense

--- src/search/main_search.cpp
+++ src/search/main_search.cpp
@@ -1,19 +1,25 @@
 #include "main_search.h"
 
+#define maxdepth 32
+
 int ply = 0;
+int nodes = 0;
+int leafNodes = 0;
+
 int pvLength[64];
 int pvTable[64][64];
 
-int searchCounter = 0;
+int searchHistory[12][64];
+int searchKillers[2][maxdepth];
+
 
 bool comp(const Move &lhs, const Move &rhs){
     return lhs.score > rhs.score;
 }
 
 
-int quiescence(Board *brd, int alpha, int beta){
-
-    searchCounter++;
+int quiescence(Board *brd, int alpha, int beta, SearchInfo *info){
+    nodes++;
 
     int score = eval(brd);
 
@@ -27,7 +33,7 @@ int quiescence(Board *brd, int alpha, int beta){
 
     // Sorting moves after score
     sort(ml.moves, ml.moves+ml.count, comp);
-
+    int legal = 0;
     /// -------------SEARCHING STARTS------------- ///
     for (int i = 0; i<ml.count; i++) {
         bool legalMove = makeMove(brd, ml.moves[i].move);
@@ -35,12 +41,19 @@ int quiescence(Board *brd, int alpha, int beta){
             continue;
         }
 
+        legal++;
         ply++;
-        int value = -quiescence(brd, -beta, -alpha);
+        int value = -quiescence(brd, -beta, -alpha, info);
         undoMove(brd);
         ply--;
 
-        if (value >= beta){ return beta; }
+        if (value >= beta){
+            if (legal == 1){
+                info->fhf++;
+            }
+            info->fh++;
+            return beta;
+        }
         if (value > alpha){
             alpha = value;
         }
@@ -49,16 +62,17 @@ int quiescence(Board *brd, int alpha, int beta){
     return alpha;
 }
 
-int askMax(Board *brd, int depth, int alpha, int beta) {
+int askMax(Board *brd, int depth, int alpha, int beta, SearchInfo *info) {
 
-    searchCounter++;
+    nodes++;
 
     pvLength[ply] = ply;
 
     if (brd->fiftyMove == 100) return 0;
 
     if (depth == 0) {
-        return quiescence(brd, alpha, beta);
+        leafNodes++;
+        return quiescence(brd, alpha, beta, info);
     }
 
     int side = brd->side;
@@ -81,20 +95,25 @@ int askMax(Board *brd, int depth, int alpha, int beta) {
     // Sorting moves after score
     sort(ml.moves, ml.moves+ml.count, comp);
 
-
     /// -------------SEARCHING STARTS------------- ///
-    bool existsLegal = false;
+    int legal = 0;
     for (int i = 0; i < ml.count; i++) {
         bool legalMove = makeMove(brd, ml.moves[i].move);
         if (!legalMove) continue;
 
-        existsLegal = true;
+        legal++;
         ply++;
-        int value = -askMax(brd, depth-1, -beta, -alpha);
+        int value = -askMax(brd, depth-1, -beta, -alpha, info);
         undoMove(brd);
         ply--;
 
-        if (value >= beta){ return beta; }
+        if (value >= beta){
+            if (legal == 1){
+                info->fhf++;
+            }
+            info->fh++;
+            return beta;
+        }
         if (value > alpha){
             alpha = value;
             bestmove = ml.moves[i].move;
@@ -109,7 +128,7 @@ int askMax(Board *brd, int depth, int alpha, int beta) {
         }
     }
 
-    if (!existsLegal) {
+    if (!legal) {
         if (side == white && isSquareAttacked(brd, brd->whiteKingPos, false)) return -mateScore + ply;
         if (side == black && isSquareAttacked(brd, brd->blackKingPos, true)) return -mateScore + ply;
         return 0;
@@ -119,14 +138,20 @@ int askMax(Board *brd, int depth, int alpha, int beta) {
 }
 
 void search(Board *brd, int depth) {
-    memset(pvLength, 0, size(pvLength));
+    memset(pvLength, 0, sizeof(pvLength));
     memset(pvTable, 0, sizeof(pvTable));
 
+    memset(searchHistory, 0, sizeof(searchHistory));
+    memset(searchKillers, 0, sizeof(searchKillers));
+
+    SearchInfo info;
+
     for (int i = 1; i <= depth; i ++) {
-        searchCounter = 0;
-        int score = askMax(brd, i, -INF, INF);
-        cout << "D" << i << ": " << searchCounter << "\n";
-        cout << "Score: " << score << "\n";
+        nodes = 0;
+        int t1 = getTime();
+        int score = askMax(brd, i, -INF, INF, &info);
+        int t2 = getTime();
+        cout << "D" << i << ": " << "nodes: " << nodes << " leafs: " << leafNodes << " score: " << score << " ordering: " << info.fhf / info.fh << " time: " << t2-t1 << "ms" << '\n';
         cout << "Pv: ";
         for (int ct = 0; ct < pvLength[0]; ct++){
             int move = pvTable[0][ct];
@@ -134,7 +159,8 @@ void search(Board *brd, int depth) {
             int toSq = toSquare(move);
             cout << "d"<< ct+1 << ": " << sqToAlgebraic(fromSq) << sqToAlgebraic(toSq) << " ";
         }
-        cout << "\n";
+        cout << "\n\n";
     }
 }
+
 
