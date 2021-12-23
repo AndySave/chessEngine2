19a914c4be08d976c98fe4a70b8be948948cc5dc
AndySave <51698537+AndySave@users.noreply.github.com>
2021-12-23 22:23:57 +0100

    Added includes

--- src/utils/helpers.h
+++ src/utils/helpers.h
@@ -4,11 +4,14 @@
 
 #include <string>
 #include "../definitions/std_typedef.h"
+#include <windows.h>
+
+
 
 int charToPiece(char c);
 char pieceToChar(int piece);
 int algebraicToSq(string &coords);
 string sqToAlgebraic(int sq);
-
+int getTime();
 
 #endif
