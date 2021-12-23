010f267ac43b8528982dcff2712b30120cbb3db5
AndySave <51698537+AndySave@users.noreply.github.com>
2021-12-23 01:49:51 +0100

    Added includes

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -8,6 +8,8 @@
 #include "../definitions/moveGeneration.h"
 #include "../evaluation/mainEvaluation.h"
 #include "../evaluation/pieceTables.h"
+#include "../search/move_ordering.h"
+#include "../search/main_search.h"
 
 
 
@@ -19,14 +21,17 @@ int main() {
     initKingAttacks();
     initBishopAttackMasks();
     initRookAttackMasks();
+
     initHashkeys();
+
+    initMvvLva();
 
     fillBishopAttacksArray();
     fillRookAttacksArray();
 
 
     Board board;
-    string fenString = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    string fenString = "6k1/5ppp/8/8/8/8/8/1K1R4 w - - 0 1";
     FEN(&board, fenString);
     printBoard(&board);
 
@@ -35,5 +40,7 @@ int main() {
     initMaterial(&board);
     initPieceTable(&board);
 
-    cout << pieceTableScoreMG << endl;
+    search(&board, 10);
 }
+
+
