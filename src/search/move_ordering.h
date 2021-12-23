727ff6cc51b864ef0359606a9ce71aedaa1c0150
AndySave <51698537+AndySave@users.noreply.github.com>
2021-12-23 06:15:19 +0100

    Added values victimScore[12] and mvvlvaScore[12][12]

--- src/search/move_ordering.h
+++ src/search/move_ordering.h
@@ -2,4 +2,13 @@
 #ifndef CHESSENGINE2_MOVE_ORDERING_H
 #define CHESSENGINE2_MOVE_ORDERING_H
 
+#include "../definitions/std_typedef.h"
+
+/// --------------------VALUES-------------------- ///
+inline constexpr int victimScore[12] = {100, 200, 300, 400, 500, 600, 100, 200, 300, 400, 500, 600};
+extern int mvvlvaScore[12][12];
+
+/// --------------------INIT FUNCTIONS-------------------- ///
+void initMvvLva();
+
 #endif
