91cbc51ef3f2d48aa2ef6e3a2c9c399008388db2
AndySave <51698537+AndySave@users.noreply.github.com>
2021-12-23 06:14:52 +0100

    Added function initMvvLva

--- src/search/move_ordering.cpp
+++ src/search/move_ordering.cpp
@@ -1,2 +1,12 @@
 
 #include "move_ordering.h"
+
+int mvvlvaScore[12][12];
+void initMvvLva(){
+    for (int attacker = P; attacker <= k; attacker++){
+        for (int victim = P; victim <= k; victim++){
+            mvvlvaScore[victim][attacker] = victimScore[victim] + 6 - (victimScore[attacker])/100;
+        }
+    }
+}
+
