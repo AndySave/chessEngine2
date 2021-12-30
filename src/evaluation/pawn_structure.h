35d4142243a6ad3ba0884800a61d5be0aa3fe697
AndySave <51698537+AndySave@users.noreply.github.com>
2021-12-31 00:26:24 +0100

    Added passedPawnBonus and passedPawnEvaluation

--- src/evaluation/pawn_structure.h
+++ src/evaluation/pawn_structure.h
@@ -6,6 +6,8 @@
 #include "../definitions/std_typedef.h"
 #include "../definitions/board.h"
 
+// TODO: Temporary. Make more advanced passed pawn handling
+inline int passedPawnBonus = 75;
 
 inline ull whitePassedPawnMasks[64];
 inline ull blackPassedPawnMasks[64];
@@ -14,6 +16,7 @@ void initWhitePassedPawnMask();
 void initBlackPassedPawnMask();
 
 bool pawnIsPassed(Board *brd, int sq, int color);
+int passedPawnEvaluation(Board *brd);
 
 
 #endif
