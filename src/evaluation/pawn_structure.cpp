d91c7c9370fba6c888f4aca2c82b07877997532c
AndySave <51698537+AndySave@users.noreply.github.com>
2021-12-31 00:09:03 +0100

    Removed initPassedPawnMask and added initWhitePassedPawnMask and black. Also added pawnIsPassed

--- src/evaluation/pawn_structure.cpp
+++ src/evaluation/pawn_structure.cpp
@@ -1,7 +1,7 @@
 
 #include "pawn_structure.h"
 
-void initPassedPawnMask(){
+void initWhitePassedPawnMask(){
     for (int sq = 0; sq < 64; sq++){
         ull bb = 0ull;
 
@@ -22,10 +22,47 @@ void initPassedPawnMask(){
             i += 8;
         }
 
-        passedPawnMasks[sq] = bb;
+        whitePassedPawnMasks[sq] = bb;
+    }
+}
+
+void initBlackPassedPawnMask(){
+    for (int sq = 0; sq < 64; sq++){
+        ull bb = 0ull;
+
+        int i = sq;
+        i -= 8;
+        while (i >= 0){
+            // There will always be a square above a pawn
+            setBit(bb, i);
+            // Check if square to the left of pawn exists
+            if (i % 8 != 0){
+                setBit(bb, i-1);
+            }
+            // Check if square to the right of pawn exists
+            if (i % 8 != 7){
+                setBit(bb, i+1);
+            }
+
+            i -= 8;
+        }
+
+        blackPassedPawnMasks[sq] = bb;
     }
 }
 
 
 
+bool pawnIsPassed(Board *brd, int sq, int color){
+    if (color == white){
+        if (!(whitePassedPawnMasks[sq] & brd->bitboards[p])){
+            return true;
+        }
+    }else{
+        if (!(blackPassedPawnMasks[sq] & brd->bitboards[P])){
+            return true;
+        }
+    }
 
+    return false;
+}
