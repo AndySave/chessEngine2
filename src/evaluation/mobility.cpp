538bd105767d0970521c7ed5a7143c2f819c19fa
ask-r0 <75451424+ask-r0@users.noreply.github.com>
2021-12-31 00:48:40 +0100

    Implemented mobilityEvaluation using common evaluation functions

--- src/evaluation/mobility.cpp
+++ src/evaluation/mobility.cpp
@@ -1,100 +1,52 @@
 #include "mobility.h"
 
 void mobilityEvaluation(Board *brd) {
-    //husk ikke bevege oppaa egene brikker
     midMobility = 0;
     endMobility = 0;
 
-    //WHITE
+    initMobility(brd);
 
-    //Knight
-    ull wKnightPositions = brd->bitboards[N];
-    while (wKnightPositions) {
-        int curKnight = getLSB(wKnightPositions);
-        clearBit(wKnightPositions, curKnight);
+    for (int i = 0; i<numOfPieces; i++) {
+        int curSq = presentPieces[i];
+        int curPiece = pieceOfSquare[curSq];
+        int curMobility = mobility[curSq];
 
-        //Counts how many non-white squares attacked
-        int knightMobility = countBits(knightAttacks[curKnight] & (~brd->occupancies[white]));
-
-        midMobility += mobilityBonus[1][knightMobility][0];
-        endMobility += mobilityBonus[1][knightMobility][1];
-    }
-
-    //Bishop
-    ull wBishopPositions = brd->bitboards[B];
-    while (wBishopPositions) {
-        int curBishop = getLSB(wBishopPositions);
-        clearBit(wBishopPositions, curBishop);
-        int bishopMobility = countBits(bishopAttackRay(brd->occupancies[both], curBishop) & (~brd->occupancies[white]));
-        midMobility += mobilityBonus[2][bishopMobility][0];
-        endMobility += mobilityBonus[2][bishopMobility][1];
-    }
-
-    //Rook
-    ull wRookPositions = brd->bitboards[R];
-    while (wRookPositions) {
-        int curRook = getLSB(wRookPositions);
-        clearBit(wRookPositions, curRook);
-        int rookMobility = countBits(rookAttackRay(brd->occupancies[both], curRook) & (~brd->occupancies[white]));
-        midMobility += mobilityBonus[3][rookMobility][0];
-        endMobility += mobilityBonus[3][rookMobility][1];
-    }
-
-    //Queen
-    ull wQueenPositions = brd->bitboards[Q];
-    while (wQueenPositions) {
-        int curQueen = getLSB(wQueenPositions);
-        clearBit(wQueenPositions, curQueen);
-        int queenMobility = countBits(queenAttackRay(brd->occupancies[both], curQueen) & (~brd->occupancies[white]));
-        midMobility += mobilityBonus[4][queenMobility][0];
-        endMobility += mobilityBonus[4][queenMobility][1];
-    }
-
-    //BLACK
-
-    //Knight
-    ull bKnightPositions = brd->bitboards[n];
-    while (bKnightPositions) {
-        int curKnight = getLSB(bKnightPositions);
-        clearBit(bKnightPositions, curKnight);
-
-        //Counts how many non-white squares attacked
-        int knightMobility = countBits(knightAttacks[curKnight] & (~brd->occupancies[black]));
-
-        midMobility -= mobilityBonus[1][knightMobility][0];
-        endMobility -= mobilityBonus[1][knightMobility][1];
-    }
-
-    //Bishop
-    ull bBishopPositions = brd->bitboards[b];
-    while (bBishopPositions) {
-        int curBishop = getLSB(bBishopPositions);
-        clearBit(bBishopPositions, curBishop);
-        int bishopMobility = countBits(bishopAttackRay(brd->occupancies[both],curBishop) & (~brd->occupancies[black]));
-        midMobility -= mobilityBonus[2][bishopMobility][0];
-        endMobility -= mobilityBonus[2][bishopMobility][1];
-
-    }
-
-    //Rook
-    ull bRookPositions = brd->bitboards[r];
-    while (bRookPositions) {
-        int curRook = getLSB(bRookPositions);
-        clearBit(bRookPositions, curRook);
-        int rookMobility = countBits(rookAttackRay(brd->occupancies[both], curRook) & (~brd->occupancies[black]));
-        midMobility -= mobilityBonus[3][rookMobility][0];
-        endMobility -= mobilityBonus[3][rookMobility][1];
-    }
-
-    //Queen
-    ull bQueenPositions = brd->bitboards[q];
-    while (bQueenPositions) {
-        int curQueen = getLSB(bQueenPositions);
-        clearBit(bQueenPositions, curQueen);
-        int queenMobility = countBits(queenAttackRay(brd->occupancies[both], curQueen) & (~brd->occupancies[black]));
-        midMobility -= mobilityBonus[4][queenMobility][0];
-        endMobility -= mobilityBonus[4][queenMobility][1];
+        switch (curPiece) {
+            case n:
+                midMobility -= mobilityBonus[1][curMobility][0];
+                endMobility -= mobilityBonus[1][curMobility][1];
+                break;
+            case b:
+                midMobility -= mobilityBonus[2][curMobility][0];
+                endMobility -= mobilityBonus[2][curMobility][1];
+                break;
+            case r:
+                midMobility -= mobilityBonus[3][curMobility][0];
+                endMobility -= mobilityBonus[3][curMobility][1];
+                break;
+            case q:
+                midMobility -= mobilityBonus[4][curMobility][0];
+                endMobility -= mobilityBonus[4][curMobility][1];
+                break;
+            case N:
+                midMobility += mobilityBonus[1][curMobility][0];
+                endMobility += mobilityBonus[1][curMobility][1];
+                break;
+            case B:
+                midMobility += mobilityBonus[2][curMobility][0];
+                endMobility += mobilityBonus[2][curMobility][1];
+                break;
+            case R:
+                midMobility += mobilityBonus[3][curMobility][0];
+                endMobility += mobilityBonus[3][curMobility][1];
+                break;
+            case Q:
+                midMobility += mobilityBonus[4][curMobility][0];
+                endMobility += mobilityBonus[4][curMobility][1];
+                break;
+            default:
+                break;
+        }
     }
 }
-
 
