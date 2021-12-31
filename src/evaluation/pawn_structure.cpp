c71967bc8cd24f32fc5b8fa9217d7cd4e903f453
AndySave <51698537+AndySave@users.noreply.github.com>
2021-12-31 14:47:37 +0100

    Added isolated pawn functions

--- src/evaluation/pawn_structure.cpp
+++ src/evaluation/pawn_structure.cpp
@@ -1,6 +1,10 @@
 
 #include "pawn_structure.h"
 
+/*
+ * Initializes passed pawn masks for white. Masks are used to determine if a
+ * pawn is passed. Must be initialized at start of program after bitMasks.
+ */
 void initWhitePassedPawnMask(){
     for (int sq = 0; sq < 64; sq++){
         ull bb = 0ull;
@@ -26,6 +30,9 @@ void initWhitePassedPawnMask(){
     }
 }
 
+/*
+ * Same as initWhitePassedPawnMask except for black pawns
+ */
 void initBlackPassedPawnMask(){
     for (int sq = 0; sq < 64; sq++){
         ull bb = 0ull;
@@ -52,7 +59,9 @@ void initBlackPassedPawnMask(){
 }
 
 
-
+/*
+ * Returns true if a pawn is passed. Requires color of the pawn you want to check
+ */
 bool pawnIsPassed(Board *brd, int sq, int color){
     if (color == white){
         if (!(whitePassedPawnMasks[sq] & brd->bitboards[p])){
@@ -67,7 +76,11 @@ bool pawnIsPassed(Board *brd, int sq, int color){
     return false;
 }
 
-
+/*
+ * Loops through all pawns on board and gives them a bonus if they are passed
+ * Returns an int score
+ * Used in main eval
+ */
 int passedPawnEvaluation(Board *brd){
     int score = 0;
 
@@ -95,4 +108,74 @@ int passedPawnEvaluation(Board *brd){
 }
 
 
+/*
+ * Initializes masks used for detecting isolated pawns
+ * Must be called at start of program
+ */
+void initIsolatedPawnMasks(){
+    for (int sq = 0; sq < 64; sq++){
+        ull bb = 0ull;
 
+        int file = sq % 8;
+
+        // Edge case for pawn on a file
+        if (file - 1 >= 0){
+            bb |= files[file-1];
+        }
+        // Edge case for pawn on h file
+        if (file + 1 < 8){
+            bb |= files[file+1];
+        }
+
+        isolatedPawnMasks[sq] = bb;
+    }
+}
+
+/*
+ * Returns true if a pawn is isolated. Requires color of the pawn you want to check
+ */
+bool pawnIsIsolated(Board *brd, int sq, int color){
+    if (color == white){
+        if (!(isolatedPawnMasks[sq] & brd->bitboards[P])){
+            return true;
+        }
+    }else{
+        if (!(isolatedPawnMasks[sq] & brd->bitboards[p])){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+/*
+ * Loops through all pawns on board and gives them a penalty if they are isolated
+ * Returns an int score
+ * Used in main eval
+ */
+int isolatedPawnEvaluation(Board *brd){
+    int score = 0;
+
+    ull whitePawns = brd->bitboards[P];
+    while (whitePawns){
+        int index = getLSB(whitePawns);
+        clearBit(whitePawns, index);
+
+        if (pawnIsIsolated(brd, index, white)){
+            score -= isolatedPawnPenalty;
+        }
+    }
+
+    ull blackPawns = brd->bitboards[p];
+    while (blackPawns){
+        int index = getLSB(blackPawns);
+        clearBit(blackPawns, index);
+
+        if (pawnIsIsolated(brd, index, black)){
+            score += isolatedPawnPenalty;
+        }
+    }
+
+    return score;
+}
