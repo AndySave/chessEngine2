36aa6975577f87c4799e3105465e154d2aeeaae2
AndySave <51698537+AndySave@users.noreply.github.com>
2022-01-01 20:40:32 +0100

    Created file

---
+++ src/evaluation/bishop.cpp
@@ -0,0 +1,6 @@
+
+#include "bishop.h"
+
+
+
+
---
+++ src/evaluation/bishop.h
@@ -0,0 +1,9 @@
+
+#ifndef CHESSENGINE2_BISHOP_H
+#define CHESSENGINE2_BISHOP_H
+
+inline ull whiteSquares = 0xAA55AA55AA55AA55;
+inline ull blackSquares = 0x55AA55AA55AA55AA;
+
+
+#endif
